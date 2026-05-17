7b59e4bb36d2c336b53fa0d2ba6fd93a83d4bede
asana <asanali1903@gmail.com>
2026-05-17 19:37:37 +0500

    main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    public static void main(String[] args) {
+
+        System.out.println("=== Small Graph (10 vertices) — Manual ===\n");
+
+        Graph smallGraph = new Graph();
+
+        for (int i = 0; i < 10; i++) {
+            smallGraph.addVertex(new Vertex(i));
+        }
+
+        smallGraph.addEdge(0, 1);
+        smallGraph.addEdge(0, 2);
+        smallGraph.addEdge(1, 3);
+        smallGraph.addEdge(1, 4);
+        smallGraph.addEdge(2, 5);
+        smallGraph.addEdge(2, 6);
+        smallGraph.addEdge(3, 7);
+        smallGraph.addEdge(4, 8);
+        smallGraph.addEdge(5, 9);
+        smallGraph.addEdge(6, 0);
+
+        smallGraph.printGraph();
+        System.out.println();
+
+        long bfsStart = System.nanoTime();
+        smallGraph.bfs(0);
+        long bfsEnd = System.nanoTime();
+        System.out.println("BFS execution time: " + (bfsEnd - bfsStart) + " ns\n");
+
+        long dfsStart = System.nanoTime();
+        smallGraph.dfs(0);
+        long dfsEnd = System.nanoTime();
+        System.out.println("DFS execution time: " + (dfsEnd - dfsStart) + " ns\n");
+
+
+        System.out.println("\n=== Performance Experiment (10 / 30 / 100 vertices) ===");
+
+        Experiment experiment = new Experiment();
+        experiment.runMultipleTests();
+        experiment.printResults();
     }
 }
